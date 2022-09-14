50b42ae020dc0082b4f1edb8973cb038b4bde169
suyanlong <syl416419@gmail.com>
2022-09-14 22:00:34 +0800

    Update version to 1.5.0-rc7

--- src/main/scala/org/ralphc/Cli.scala
+++ src/main/scala/org/ralphc/Cli.scala
@@ -2,14 +2,33 @@ package org.ralphc
 
 import java.util.concurrent.Callable
 import picocli.CommandLine.{Command, Option}
+import org.alephium.protocol.vm.lang.CompilerOptions
 
-@Command(name = "ralphc", mixinStandardHelpOptions = true, version = Array("ralphc 1.5.0-rc6"), description = Array("compiler ralph language."))
+@Command(name = "ralphc", mixinStandardHelpOptions = true, version = Array("ralphc 1.5.0-rc7"), description = Array("compiler ralph language."))
 class Cli extends Callable[Int] {
   @Option(names = Array("-f"))
   val files: Array[String] = Array.empty
 
   @Option(names = Array("-d", "--debug"), defaultValue = "false", description = Array("debug mode"))
   var debug: Boolean = false
+
+  @Option(names = Array("--ic"), defaultValue = "false", description = Array("ignore unused constants warning"))
+  var ignoreUnusedConstantsWarnings: Boolean = false
+
+  @Option(names = Array("--iv"), defaultValue = "false", description = Array("ignore unused variables warning"))
+  var ignoreUnusedVariablesWarnings: Boolean = false
+
+  @Option(names = Array("--if"), defaultValue = "false", description = Array("ignore unused fields warning"))
+  var ignoreUnusedFieldsWarnings: Boolean = false
+
+  @Option(names = Array("--ir"), defaultValue = "false", description = Array("ignore readonly check warning"))
+  var ignoreReadonlyCheckWarnings: Boolean = false
+
+  @Option(names = Array("--ip"), defaultValue = "false", description = Array("ignore unused private functions warning"))
+  var ignoreUnusedPrivateFunctionsWarnings: Boolean = false
+
+  @Option(names = Array("--ie"), defaultValue = "false", description = Array("ignore external call check warning"))
+  var ignoreExternalCallCheckWarnings: Boolean = false
 
   def _print[O](other: O): Int = {
     if (debug) {
@@ -35,14 +54,26 @@ class Cli extends Callable[Int] {
   }
 
   override def call(): Int = {
+
+    val compilerOptions = CompilerOptions(
+      ignoreUnusedConstantsWarnings = ignoreUnusedConstantsWarnings,
+      ignoreUnusedVariablesWarnings = ignoreUnusedVariablesWarnings,
+      ignoreUnusedFieldsWarnings = ignoreUnusedFieldsWarnings,
+      ignoreReadonlyCheckWarnings = ignoreReadonlyCheckWarnings,
+      ignoreUnusedPrivateFunctionsWarnings = ignoreUnusedPrivateFunctionsWarnings,
+      ignoreExternalCallCheckWarnings = ignoreExternalCallCheckWarnings
+    )
+
     val rets = for {
       path <- files
       ret = Parser
         .Parser(path)
         .fold(
           deps => error("circular dependency：\n" + deps.mkString("\n"), path),
-          value => value._1.fold(_ => Compiler.compileScript(value._2).fold(err => error(err.detail, value), ret => ok(ret.bytecodeTemplate, value)))
-                                (_ => Compiler.compileContract(value._2).fold(err => error(err.detail, value), ret => ok(ret.bytecode, value)))
+          value =>
+            value._1.fold(_ =>
+              Compiler.compileScript(value._2, compilerOptions).fold(err => error(err.detail, value), ret => ok(ret.bytecodeTemplate, value))
+            )(_ => Compiler.compileContract(value._2, compilerOptions).fold(err => error(err.detail, value), ret => ok(ret.bytecode, value)))
         )
     } yield ret
     rets.sum
--- src/main/scala/org/ralphc/Compiler.scala
+++ src/main/scala/org/ralphc/Compiler.scala
@@ -1,24 +1,34 @@
 package org.ralphc
 
 import org.alephium.api.{Try, failed}
-import org.alephium.api.model.{CompileContractResult, CompileScriptResult}
+import org.alephium.api.model.{CompileContractResult, CompileScriptResult, CompileProjectResult}
+import org.alephium.protocol.vm.lang.CompilerOptions
 import org.alephium.protocol.vm.lang
 
 object Compiler {
   @SuppressWarnings(Array("org.wartremover.warts.ToString"))
-  def compileScript(code: String): Try[CompileScriptResult] = {
+  def compileScript(code: String, compilerOptions: CompilerOptions = CompilerOptions.Default): Try[CompileScriptResult] = {
     lang.Compiler
-      .compileTxScriptFull(code)
+      .compileTxScriptFull(code, compilerOptions = compilerOptions)
       .map(p => CompileScriptResult.from(p._1, p._2, p._3))
       .left
       .map(error => failed(error.toString))
   }
 
   @SuppressWarnings(Array("org.wartremover.warts.ToString"))
-  def compileContract(code: String): Try[CompileContractResult] = {
+  def compileContract(code: String, compilerOptions: CompilerOptions = CompilerOptions.Default): Try[CompileContractResult] = {
     lang.Compiler
-      .compileContractFull(code)
+      .compileContractFull(code, compilerOptions = compilerOptions)
       .map(p => CompileContractResult.from(p._1, p._2, p._3))
+      .left
+      .map(error => failed(error.toString))
+  }
+
+  @SuppressWarnings(Array("org.wartremover.warts.ToString"))
+  def compileProject(code: String, compilerOptions: CompilerOptions = CompilerOptions.Default): Try[CompileProjectResult] = {
+    lang.Compiler
+      .compileProject(code, compilerOptions = compilerOptions)
+      .map(p => CompileProjectResult.from(p._1, p._2))
       .left
       .map(error => failed(error.toString))
   }
